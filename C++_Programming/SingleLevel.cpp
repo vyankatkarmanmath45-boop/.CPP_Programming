f6f8a0f08a94bd32dd211f0622c24a4f65513bf0
Vyankatkar Manmath <vyankatkarmanmath45@gmail.com>
2026-05-04 00:07:05 +0530

    Learning Inheritance

---
+++ C++_Programming/SingleLevel.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+class Base
+{
+    public:
+        int i,j;
+
+        Base()
+        {
+            cout<<"Inside Base Constructor\n";
+            i=0;
+            j=0;
+        }
+
+        ~Base()
+        {
+            cout<<"Inside Base Destructor\n";
+        }
+
+        void fun()
+        {
+            cout<<"Inside Base Fun\n";
+        }
+
+};
+
+class Derived : public Base
+{
+
+};
+
+int main()
+{
+    Base bobj;
+
+    //Derived dobj;
+
+    return 0;
+}
